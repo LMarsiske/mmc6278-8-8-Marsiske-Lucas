320d7989948bbeb7c83066284694c010ce94e16d
Lucas Marsiske <47642996+LMarsiske@users.noreply.github.com>
2022-06-29 13:14:44 -0400

    Remembered I couldn't use .env in vanillaJS, got API response converted to json

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // Your code here
 const form = document.querySelector("form");
 const input = document.getElementById("weather-search");
+const api_key = "4237191eb462a237532ce11fc2d03958";
 
-form.onsubmit = (e) => {
+form.onsubmit = async (e) => {
   e.preventDefault();
+
+  console.log(input.value);
+  let response = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?units=imperial&appid=${api_key}&q=${input.value}`
+  );
+
+  let data = await response.json();
+  console.log(data);
 };
